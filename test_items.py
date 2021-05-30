e3ecfd159248013238c3d114aa3358d1db09f324
YLIDKO <artyom_litvin@tut.by>
2021-05-30 23:56:15 +0300

    func name

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 
-def test_guest_should_see_login_link_pass(browser):
+def test_find__card_button(browser):
     browser.get(link)
     time.sleep(30)
     assert browser.find_element_by_css_selector(".btn-add-to-basket") == True, "Wrong language"
