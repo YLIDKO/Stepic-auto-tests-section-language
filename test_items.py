5e3090a69c8164b90ea9d43fc4f028b7626e85de
YLIDKO <artyom_litvin@tut.by>
2021-05-30 23:29:18 +0300

    test file

---
+++ test_items.py
@@ -0,0 +1,10 @@
+import time
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+
+def test_guest_should_see_login_link_pass(browser):
+    browser.get(link)
+    time.sleep(3)
+    assert browser.find_element_by_css_selector(".btn-add-to-basket")
+
